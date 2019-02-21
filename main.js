504a9b3efc2df34ac8df4f7346ee98125ee4a699
Roman Romanov <roman.romanov1998@gmail.com>
2019-02-21 14:31:30 +0000

    Added a registration page

---
+++ main.js
@@ -0,0 +1,17 @@
+// function to open the side navigation
+function openNav() {
+  document.getElementById("mySidenav").style.width = "200px";
+}
+//function to close side navigation
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// open popup login box
+function openForm() {
+  document.getElementById("background-container").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("background-container").style.display = "none";
+}
