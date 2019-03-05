9e1e9da29d20ba0966af6910b3c790a6e77d5dd1
Roman Romanov <roman.romanov1998@gmail.com>
2019-03-05 14:34:05 +0000

    Created shared nav, footer, login box html files. Rearranged code in html and css files.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+$(function() {
+  var navigation = $('#navigation');
+  if(window.location.pathname == '/index.html') {
+    navigation.load('pages/shared/nav.html');
+  } else {
+    navigation.load('shared/nav.html');
+  }
+
+
+  var footer = $('#footer');
+  if(window.location.pathname == '/index.html') {
+    footer.load('pages/shared/footer.html');
+  } else {
+    footer.load('shared/footer.html');
+  }
+
+
+  var loginBox = $('#loginBox');
+  if(window.location.pathname == '/index.html') {
+    loginBox.load('pages/shared/login_box.html');
+  } else {
+    loginBox.load('shared/login_box.html');
+  }
+
+});
+
 // function to open the side navigation
 function openNav() {
   document.getElementById("mySidenav").style.width = "200px";
